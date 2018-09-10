c45f30618c4b8875a85359b1343ccbf888a53e6d
Daniel Schoepe <schoepe@amazon.com>
2018-09-09 21:50:04 -0700

    Document proof steps in ETM proof

--- src/Quivela/Examples/ETM.hs
+++ src/Quivela/Examples/ETM.hs
@@ -79,7 +79,7 @@ _mac = adversary()
           cpa_h = fieldEqual ["cpa", "h"]
   in
   [prog| EtM(_e,_mac) |]
-  ≈
+  ≈ Hint [Note "Unfold definition of EtM"]:
   [prog|
 new (const e=_e, const mac=_mac) {
     method enc(a, m) { m & em = e.enc(m) & t = mac.tag(<a,em>) & <em,t> }
@@ -92,19 +92,19 @@ new (const e=_e, const mac=MacI(_mac)) {
     method enc(a, m) { m & em = e.enc(m) & t = mac.tag(<a,em>) & <em,t> }
     method dec(a, c) { em = c`0, t = c`1, c & mac.verify(<a,em>,t) & e.dec(em) }
 }|]
-   ≈
+   ≈ Hint [Note "Introducing cpa"]:
    [prog|
 new (..., const cpa=new(const e=_e) {
         method enc(m) { e.enc(m) }
         method dec(c) { e.dec(c) }
     }) { ... }
 |]
-   ≈
+   ≈ Hint [Note "Replace calls to e by calls to cpa"]:
    [prog|
     method enc(a, m) { m & em = cpa.enc(m) & t = mac.tag(<a,em>) & <em,t> }
     method dec(a, c) { em = c`0, t = c`1, c & mac.verify(<a,em>,t) & cpa.dec(em) }
 |]
-   ≈
+   ≈ Hint [Note "Store ciphertext in h map"]:
    [prog|
 new (..., const cpa=new(const e=_e,h=0) {
         method enc(m) { c = e.enc(m) & h[c] = 1 & c }
@@ -112,11 +112,11 @@ new (..., const cpa=new(const e=_e,h=0) {
     }) { ...
     invariant inv1(a, em) { !mac.tg[<a, em>] | cpa.h[em] }
 }|]
-   ≈ -- Hint [mac_tg, cpa_h ]:
+   ≈ Hint [Note "Ensure ciphertext in h before decryption"]:
    [prog|
     method dec(a, c) { em = c`0, t = c`1, c & mac.verify(<a,em>,t) & cpa.h[em] & cpa.dec(em) }
 |]
-   ≈
+   ≈ Hint [Note "Move h lookup into cpa object"]:
    [prog|
 new (..., const cpa=new(const e=_e,h=0) {
         method enc(m) { c = e.enc(m) & h[c] = 1 & c }
@@ -125,23 +125,23 @@ new (..., const cpa=new(const e=_e,h=0) {
     method dec(a, c) { em = c`0, t = c`1, c & mac.verify(<a,em>,t) & cpa.dec(em) }
     delete inv1
 }|]
-   ≈ -- [mac_tg, cpa_h]:
+   ≈ Hint [Note "Fold definition of CpaC"]:
    [prog|
 new (..., const cpa=CpaC(_e)) { ... }|]
-   ≈
+   ≈ Hint [Note "Delete unused e field"]:
    [prog|
 new (..., delete e) { ... }|]
    ≈ Hint [rewrite "CpaC(_e)" "CpaI(_e)"]:
    [prog|
 new (..., const cpa=CpaI(_e)) { ... } |]
-  ≈
+  ≈ Hint [Note "Reintroduce e"]:
   [prog|
 new (..., const e=_e) { ... }|]
-  ≈
+  ≈ Hint [Note "Look up ciphertext in map instead of calling decrypt"]:
   [prog|
     method enc(a, m) { m & em = e.enc(Z(m)) & cpa.d[em] = m & t = mac.tag(<a,em>) & <em,t> }
     method dec(a, c) { em = c`0, t = c`1, c & mac.verify(<a,em>,t) & cpa.d[em] } |]
-   ≈
+   ≈ Hint [Note "Introduce d"]:
    [prog|
 new (..., d=0) {...
     method enc(a, m) { m & em = e.enc(Z(m)) & cpa.d[em] = m & t = mac.tag(<a,em>) & d[<a,<em,t>>] = m & <em,t> }
@@ -152,27 +152,20 @@ new (const mac=MacI(_mac),const cpa=CpaI(Enc(_e)), const e=Enc(_e), d=0) {
     method enc(a,m) { m & em = e.enc(Z(m)) & cpa.d[em] = m & t = mac.tag(<a,em>) & d[<a,<em,t>>] = m & <em,t> }
     method dec(a,c) { em = c`0, t = c`1, c & mac.verify(<a,em>,t) & cpa.d[em] }
 }|]
-    ≈
-      [prog|
-     method dec(a:*,c:<*,*>) {
-         em = c`0,
-         t = c`1,
-         c &
-         mac.verify(<a,em>,t) &  // t & mac.tg[<a,em>] == t
-         cpa.d[em] &
-         d[<a,<em,t>>]  // !d[<a,<em,t>>] | (t == mac.tg[<a,em>] & cpa.d[em] == d[<a,<em,t>>])
-     }
+    ≈ Hint [Note "Look up ciphertext in new d map"]:
+    [prog|
+     method dec(a:*,c:<*,*>) { em = c`0, t = c`1, c & mac.verify(<a,em>,t) & cpa.d[em] & d[<a,<em,t>>] }
      invariant inv1(a, em, t) { d[<a, <em, t>>] == (t & (mac.tg[<a, em>] == t) & cpa.d[em]) }
      invariant inv2(a, em, t) { (!mac.tg[<a, em>]) | (cpa.d[em] & e.d[em]) }
 |]
-    ≈ Hint [ NoInfer, mac_tg, e_d, d, cpa_d ]:
+    ≈ Hint [Note "Lookup in d to decrypt", NoInfer, mac_tg, e_d, d, cpa_d ]:
      [prog|
       delete inv1
       delete inv2
       method dec(a, c) { d[<a,c>] }
       invariant inv3(a, em, t) { (!d[<a, <em, t>>]) | (t & ((t == mac.tg[<a, em>]) & (cpa.d[em] == d[<a, <em, t>>])) & e.d[em]) }
 |]
-    ≈ Hint [NoInfer, mac_tg, e_d, d, cpa_d]:
+    ≈ Hint [Note "Drop invariant", NoInfer, mac_tg, e_d, d, cpa_d]:
      [prog|
       delete inv3
 |]
@@ -182,7 +175,7 @@ new (const mac=MacI(_mac),const cpa=CpaI(Enc(_e)), const e=Enc(_e), d=0) {
       method enc(a, m) { m & em = e.enc(Z(m)) & cpa.d[em] = m & t = mac.tag(<a,em>) & d[<a,<em,t>>] = m & <em,t> }
       method dec(a, c) { d[<a,c>] }
   }|]
-    ≈ -- [mac_tg, d]:
+    ≈ Hint [Note "cpa unused"]:
     [prog|
   new (..., delete cpa) {...
       method enc(a, m) { m & em = e.enc(Z(m)) & em & t = mac.tag(<a,em>) & d[<a,<em,t>>] = m & <em,t> }
@@ -193,7 +186,7 @@ new (const mac=MacI(_mac),const cpa=CpaI(Enc(_e)), const e=Enc(_e), d=0) {
       method enc(a, m) { m & em = e.enc(Z(m)) & em & t = mac.tag(<a,em>) & d[<a,<em,t>>] = m & <em,t> }
       method dec(a, c) { d[<a,c>] }
   }|]
-    ≈ -- Hint [d]:
+    ≈ Hint [Note "Factor out operations into e"]:
     [prog|
   new (const e=(new(const e=_e,const mac=_mac) {
           method enc(a, m) { m & em = e.enc(m) & em & t = mac.tag(<a,em>) & <em,t> }
