663db077b608d61317e5e8ffd8a5e3ec3884c7ba
Daniel Schoepe <schoepe@amazon.com>
2018-09-16 16:10:03 -0700

    Prove security of envelope encryption (almost)
    This commit adds a proof for envelope encryption; there's a minor
    missing part stemming from having two types of constructors: ordinary
    methods and typed ones.

--- src/Quivela/Examples/Envelope.hs
+++ src/Quivela/Examples/Envelope.hs
@@ -1,18 +1,30 @@
 {-# LANGUAGE QuasiQuotes, TemplateHaskell #-}
-module Env where
+module Envelope where
 
 import Control.Lens hiding (rewrite)
+import qualified Data.Map as M
 import Quivela
 
 prove (set useCache False emptyVerifyEnv)
   [prog'|
-method skenc(k, a, m) { rnd() }
-method skdec(k, a, c) { rnd() }
+function skenc(k, a, m)
+function skdec(k, a, c)
 
-method Enc(e) { new (const e,d=0) {
+
+method Enc(e) { new (const e=e,d=0) {
    method enc(a,m)      { c = e.enc(a,Z(m)) & d[a][c] = <m> & c } // do we need to add the !d[a][c] check?
    method dec(a,c)      { d[a][c] }
 }}
+
+// FIXME: We should unify these "strongly typed" constructors and the existing ones
+type ET = new (const k) {
+   method enc(a,m)      { skenc(k,a,m) }
+   method dec(a,c)      { skdec(k,a,c) }
+}
+type EncT = new (const e: ET, d=0) {
+   method enc(a,m)      { c = e.enc(a,Z(m)) & d[a][c] = <m> & c } // do we need to add the !d[a][c] check?
+   method dec(a,c)      { d[a][c] }
+}
 
 method E() { new (const k=rnd()) {
    method enc(a,m)      { skenc(k,a,m) }
@@ -29,7 +41,7 @@ method Env(e) { new (const e=e) {
 // th: Enc(Env(Enc(e))) ~ Env(Enc(e)) {
 
 
-method EncL(e) { new (const e=e,d) {
+method EncL(e) { new (const e=e,d=0) {
     method enc(a,m)      { c = e.enc(a,Z(m)) & d[a][c] = <m> & c }
 }}
 
@@ -67,134 +79,100 @@ new (const e = EncLI(e),d=0) {
    ≈ Hint [Note "def e.enc; Z(k) ~ zk"]:
    [prog|
 new (const e = EncLI(e),d=0) {
-  method enc(a,m)      { k=rnd() & ek = e.e.enc(0,Z(k)) & em = skenc(k,a,Z(m)) & c = <ek,em> & d[a][c] = <m> & c }
+  method enc(a,m)      { k=rnd(), zk = Z(k) & ek = e.enc(0,zk) & em = skenc(k,a,Z(m)) & c = <ek,em> & d[a][c] = <m> & c }
 // orig: enc(a,m)      { k=rnd() & ek = e.enc(0,zk) & em = skEnc(k,a,Z(m)) & c = <ek,em> & d[a][c] = <m> & c }
   method dec(a,c)      { d[a][c] }
 }|]
   ≈ Hint [Note "introducing f; def E"]:
   [prog|
 new (const e = EncLI(e),d=0) {
-  method enc(a,m)      { f = E() & ek = e.e.enc(0,Z(f.k)) & em = f.enc(a,Z(m)) & c = <ek,em> & d[a][c] = <m> & c }
+  method enc(a,m)      { zk = Z(rnd()), f = E() & ek = e.enc(0,zk) & em = f.enc(a,Z(m)) & c = <ek,em> & d[a][c] = <m> & c }
 // orig: enc(a,m)      { f = E() & ek = e.enc(0,zk) & em = f.enc(a,Z(m)) & c = <ek,em> & d[a][c] = <m> & c }
   method dec(a,c)      { d[a][c] }
 }|]
-  ≈ Hint [Note "assumption", rewrite "E()" "Enc(E())"]:
+  ≈ Hint [rewrite "E()" "Enc(E())"]:
   [prog|
 new (const e = EncLI(e),d=0) {
-  method enc(a,m)      { f = Enc(E()) & ek = e.e.enc(0,Z(f.k)) & em = f.enc(a,Z(m)) & c = <ek,em> & d[a][c] = <m> & c }
+  method enc(a,m)      { zk = Z(rnd()), f = Enc(E()) & ek = e.enc(0,zk) & em = f.enc(a,Z(m)) & c = <ek,em> & d[a][c] = <m> & c }
   method dec(a,c)      { d[a][c] }
 }|]
-  ≈ Hint [Note "similar to above (f.dec not called) - replace Enc(E()) w EncLI(E()), then Z(m) w. m, then EncLI(E()) w. Enc(E())"]:
+  ≈ Hint [ Note "similar to above (f.dec not called) - replace Enc(E()) w EncLI(E()), then Z(m) w. m, then EncLI(E()) w. Enc(E())" ]:
   [prog|
 new (const e = EncLI(e),d=0) {
-  method enc(a,m)      { f = Enc(E()) & ek = e.e.enc(0,Z(f.k)) & em = f.enc(a,m) & c = <ek,em> & d[a][c] = <m> & c }
+  method enc(a,m)      { zk = Z(rnd()), f = Enc(E()) & ek = e.enc(0,zk) & em = f.enc(a,m) & c = <ek,em> & d[a][c] = <m> & c }
   method dec(a,c)      { d[a][c] }
 }|]
-     :[]
-     {-
-~ // introducing fs
-
-new (e = EncLI(e),d=0,fs=0) {
-
-       enc(a,m)      { f = Enc(E()) & ek = e.enc(0,zk) & em = f.enc(a,m) & fs[ek]=f & c = <ek,em> & d[a][c] = <m> & c }
-
-       dec(a,c)      { d[a][c] }
-
-}
-
-~ // invariant(a,c) { !d[a][c] | <ek,em> = d[a][c] & 1 }
-
-new (e = EncLI(e),d=0,fs=0) {
-
-       enc(a,m)      { f = Enc(E()) & ek = e.enc(0,zk) & em = f.enc(a,m) & fs[ek]=f & c = <ek,em> & d[a][c] = <m> & c }
-
-       dec(a,c)      { <ek,em> = c & d[a][c] }
-
-}
-
-~ // invariant(a,ek,em) { d[a][<ek,em>] == fs[ek].d[a][em] }
-
-new (e = EncLI(e),fs=0) {
-
-       enc(a,m)      { f = Enc(E()) & ek = e.enc(0,zk) & em = f.enc(a,m) & fs[ek]=f & c = <ek,em> & d[a][c] = <m> & c }
-
-       dec(a,c)      { <ek,em> = c & fs[ek].dec(a,em) }
-
-}
-
-~ // d auxilliary
-
-new (e = EncLI(e),fs=0) {
-
-       enc(a,m)      { f = Enc(E()) & ek = e.enc(0,zk) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
-
-       dec(a,c)      { <ek,em> = c & fs[ek].dec(a,em) }
-
-}
-
-~ // Enc(E()) ~ E()
-
-new (e = EncLI(e),fs=0) {
-
-       enc(a,m)      { f = E() & ek = e.enc(0,zk) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
-
-       dec(a,c)      { <ek,em> = c & fs[ek].dec(a,em) }
-
-}
-
-~ // zk ~ Z(f.k)
-
-new (e = EncLI(e),fs=0) {
-
-       enc(a,m)      { f = E() & ek = e.enc(0,f.k) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
-
-       dec(a,c)      { <ek,em> = c & fs[ek].dec(a,em) }
-
-}
-
-~ // EncLI(e) ~ EncL(e); switching from EncL to Enc
-
-new (e = EncLI(e),fs=0) {
-
-       enc(a,m)      { f = E() & ek = e.enc(0,f.k) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
-
-       dec(a,c)      { <ek,em> = c & fs[ek].dec(a,em) }
-
-}
-
-~ // invariant(ek) { fs[ek].k == e.d[0][ek]) }; type of fs
-
-new (e = Enc(e),fs=0) {
-
-       enc(a,m)      { f = E() & ek = e.enc(0,f.k) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
-
-       dec(a,c)      { <ek,em> = c & k = e.dec(0,ek) & skDec(k,a,em) }
-
-}
-
-~ // fs auxilliary
-
-new (e = Enc(e)) {
-
-       enc(a,m)      { f = E() & ek = e.enc(0,f.k) & em = f.enc(a,m) & <ek,em> }
-
-       dec(a,c)      { <ek,em> = c & k = e.dec(0,ek) & skDec(k,a,em) }
-
-}
-
-~ // inlining
-
-new (e = Enc(e)) {
-
-       enc(a,m)      { k = rnd() & ek = e.enc(0,k) & em = skEnc(k,a,m) & <ek,em> }
-
-       dec(a,c)      { <ek,em> = c & k = e.dec(0,ek) & skDec(k,a,em) }
-
-}
-
-~ // defs
-
-Env(Enc(e))
-
-       }
--}
+  ≈ Hint [Note "introduce fs, move to EncE()"]:
+  [prog|
+new (const e = EncLI(e),d=0, fs: map * EncT = 0) {
+  method enc(a,m)      { zk = Z(rnd()), f = new EncT(e= new ET(k=rnd())) & ek = e.enc(0,zk) & em = f.enc(a,m) & fs[ek]=f & c = <ek,em> & d[a][c] = <m> & c }
+  method dec(a,c: <*, *>) { d[a][c] } // todo: get rid of type annotation
+  invariant i1(a,c)    { !d[a][c] | <ek,em> = d[a][c] & 1 }
+  invariant i2(a,ek,em){ d[a][<ek,em>] == fs[ek].d[a][em] }
+}|]
+  ≈ Hint [Note "address bijection doesn't remap result values"]:
+  [prog|
+   method dec(a,c)      { <ek,em> = c & fs[ek].dec(a, em) } |]
+  ≈ Hint [Note "d auxiliary"]:
+  [prog|
+new (...,delete d) {...
+  delete i1
+  delete i2
+  method enc(a,m)      { zk = Z(rnd()), f = new EncT(e=new ET(k=rnd())) & ek = e.enc(0,zk) & em = f.enc(a,m) & fs[ek]=f & c = <ek,em> & c }
+}|]
+  ≈ Hint [Note "tbd: unify EncT and Enc", Admit]:
+  [prog|
+new (const e = EncLI(e),fs:map * ET=0) {
+    method enc(a,m)    { zk = Z(rnd()), f = new ET(k=rnd()) & ek = e.enc(0,zk) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
+    method dec(a,c)    { <ek,em> = c & fs[ek].dec(a,em) }
+}|]
+  ≈ Hint [Note "move from zk to f.k"]:
+  [prog|
+new (const e = EncLI(e),fs:map * ET=0) {
+    method enc(a,m)    { zk = Z(rnd()), f = new ET(k=rnd()) & ek = e.enc(0,f.k) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
+    method dec(a,c)    { <ek,em> = c & fs[ek].dec(a,em) }
+}|]
+  ≈ Hint [Note "Drop zk"]:
+  [prog|
+new (const e = EncLI(e),fs:map * ET=0) {
+    method enc(a,m)    { f = new ET(k=rnd()) & ek = e.enc(0,f.k) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
+    method dec(a,c)    { <ek,em> = c & fs[ek].dec(a,em) }
+}|]
+  ≈ Hint []:
+  [prog|
+new (const e = EncL(e),fs:map * ET=0) {
+    method enc(a,m)    { f = new ET(k=rnd()) & ek = e.enc(0,f.k) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
+    method dec(a,c)    { <ek,em> = c & fs[ek].dec(a,em) }
+}|]
+  ≈ Hint [Note "add inv1"]:
+  [prog|
+new (const e = EncL(e),fs:map * ET=0) {
+    method enc(a,m)    { f = new ET(k=rnd()) & ek = e.enc(0,f.k) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
+    method dec(a,c)    { <ek,em> = c & fs[ek].dec(a,em) }
+    invariant i1(ek)   { !fs[ek]  | fs[ek].k == e.d[0][ek] } // type of fs
+}|]
+  ≈ Hint [Note "Move from fs[..].dec to using e and skdec"]:
+  [prog|
+new (const e = Enc(e),fs:map * ET=0) {
+   method enc(a,m)    { f = new ET(k=rnd()) & ek = e.enc(0,f.k) & em = f.enc(a,m) & fs[ek]=f & <ek,em> }
+   method dec(a,c)    { <ek,em> = c & k = e.dec(0,ek) & skdec(k,a,em) }
+   invariant i1(ek)   { !fs[ek]  | fs[ek].k == e.d[0][ek] } // type of fs
+   invariant i2(ek)   { !(fs[ek] == 0) | !e.d[0][ek] }
+   invariant i3(ek)   { !(e.d[0][ek] == 0) | !fs[ek] }
+}|]
+  ≈ Hint [Note "fs auxiliary"]:
+  [prog|
+new (const e = Enc(e)) {
+   method enc(a,m)         { f = new ET(k=rnd()) & ek = e.enc(0,f.k) & em = f.enc(a,m) & <ek,em> }
+   method dec(a,c)         { <ek,em> = c & k = e.dec(0,ek) & skdec(k,a,em) }
+}|]
+  ≈ Hint [Note "inlining"]:
+  [prog|
+new (const e = Enc(e)) {
+   method enc(a,m)         { k=rnd() & ek = e.enc(0,k) & em = skenc(k, a, m) & <ek,em> }
+   method dec(a,c)         { <ek,em> = c & k = e.dec(0,ek) & skdec(k,a,em) }
+}|]
+  ≈ Hint [Note "fold definitions"]:
+  [prog|
+Env(Enc(e)) |]
+  : []
