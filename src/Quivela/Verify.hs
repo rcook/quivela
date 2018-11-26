2c4885a607aa3f7cccadd39158cf86bf67b0e013
Sean McLaughlin <seanmcl@gmail.com>
2018-11-26 15:50:22 -0800

    Add license header generator
    cr https://code.amazon.com/reviews/CR-3994127

--- src/Quivela/Verify.hs
+++ src/Quivela/Verify.hs
@@ -1,3 +1,5 @@
+-- Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+-- SPDX-License-Identifier: Apache-2.0
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TemplateHaskell #-}
